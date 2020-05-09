1038a5b9abbf6d6fe05ae5fc14b4b4ed82ea939a
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2020-05-09 17:51:52 -0400

    added Where To Eat project

--- index.js
+++ index.js
@@ -1,155 +1,155 @@
 "use strict"
 
-function handleAboutBtn() {    
+function handleAboutBtn() {
 
-    $('.js-about-btn').on('click', function() {
-        
+    $('.js-about-btn').on('click', function () {
+
         $(".main-home").hide();
         $(".work-content").hide();
         $(".resume-content").hide();
-        $(".footer-content").hide();      
-        $(".about-content").show();        
+        $(".footer-content").hide();
+        $(".about-content").show();
         $(".footer-home").show();
-       
-        $(".js-about-btn").css({"text-decoration-line": "line-through", "font-style": "italic"});
-        $(".js-work-btn").css({"text-decoration-line": "none", "font-style": "normal"});
-        $(".js-resume-btn").css({"text-decoration-line": "none", "font-style": "normal"});
+
+        $(".js-about-btn").css({ "text-decoration-line": "line-through", "font-style": "italic" });
+        $(".js-work-btn").css({ "text-decoration-line": "none", "font-style": "normal" });
+        $(".js-resume-btn").css({ "text-decoration-line": "none", "font-style": "normal" });
         $("#line-separator").css("display", "none");
-        $("body").css("background-color", "#CCD1D1");        
+        $("body").css("background-color", "#CCD1D1");
         $("header button").css("position", "static");
     });
-        
-    $('#about-close-btn').on('click', function() {
+
+    $('#about-close-btn').on('click', function () {
         $(".about-content").hide();
         $(".footer-content").hide();
         $(".main-home").show();
         $(".footer-home").show();
 
-        $(".js-about-btn").css({"text-decoration-line": "none", "font-style": "normal"});
+        $(".js-about-btn").css({ "text-decoration-line": "none", "font-style": "normal" });
         $("#line-separator").css("display", "none");
         $("body").css("background-color", "#EF5350");
         $("#background-whitish").css("display", "block");
-    });   
+    });
 }
 
 
-function handleResumeBtn() {    
+function handleResumeBtn() {
 
-    $('.js-resume-btn').on('click', function() {
-        
+    $('.js-resume-btn').on('click', function () {
+
         $(".main-home").hide();
         $(".about-content").hide();
         $(".work-content").hide();
-        $(".footer-content").hide();      
-        $(".resume-content").show();        
+        $(".footer-content").hide();
+        $(".resume-content").show();
         $(".footer-home").show();
-       
-        $(".js-resume-btn").css({"text-decoration-line": "line-through", "font-style": "italic"});
-        $(".js-work-btn").css({"text-decoration-line": "none", "font-style": "normal"});
-        $(".js-about-btn").css({"text-decoration-line": "none", "font-style": "normal"});
+
+        $(".js-resume-btn").css({ "text-decoration-line": "line-through", "font-style": "italic" });
+        $(".js-work-btn").css({ "text-decoration-line": "none", "font-style": "normal" });
+        $(".js-about-btn").css({ "text-decoration-line": "none", "font-style": "normal" });
         $("#line-separator").css("display", "none");
-        $("body").css("background-color", "#CCD1D1");      
+        $("body").css("background-color", "#CCD1D1");
         $("header button").css("position", "static");
-    }); 
-        
-    $('#resume-close-btn').on('click', function() {
+    });
+
+    $('#resume-close-btn').on('click', function () {
         $(".resume-content").hide();
         $(".footer-content").hide();
         $(".main-home").show();
         $(".footer-home").show();
 
-        $(".js-resume-btn").css({"text-decoration-line": "none", "font-style": "normal"});
+        $(".js-resume-btn").css({ "text-decoration-line": "none", "font-style": "normal" });
         $("#line-separator").css("display", "none");
         $("body").css("background-color", "#EF5350");
-        $("#background-whitish").css("display", "block");                 
+        $("#background-whitish").css("display", "block");
     });
 }
 
 
-function handleWorkBtn() {   
+function handleWorkBtn() {
 
-    $('.js-work-btn').on('click', function() {        
+    $('.js-work-btn').on('click', function () {
 
         $(".main-home").hide();
         $(".about-content").hide();
-        $(".resume-content").hide(); 
-        $(".footer-content").hide();       
-        $(".work-content").show();        
+        $(".resume-content").hide();
+        $(".footer-content").hide();
+        $(".work-content").show();
         $(".footer-home").show();
 
-        $(".js-work-btn").css({"text-decoration-line": "line-through", "font-style": "italic"});
-        $(".js-about-btn").css({"text-decoration-line": "none", "font-style": "normal"});
-        $(".js-resume-btn").css({"text-decoration-line": "none", "font-style": "normal"});
+        $(".js-work-btn").css({ "text-decoration-line": "line-through", "font-style": "italic" });
+        $(".js-about-btn").css({ "text-decoration-line": "none", "font-style": "normal" });
+        $(".js-resume-btn").css({ "text-decoration-line": "none", "font-style": "normal" });
         $("body").css("background-color", "#CCD1D1");
         $("#line-separator").css("display", "none");
-        $("main").css({"margin-bottom": "50px"});
-        $(".projects button").css("display", "block");          
+        $("main").css({ "margin-bottom": "50px" });
+        $(".projects button").css("display", "block");
     });
 
-    $('#work-close-btn').on('click', function() {
+    $('#work-close-btn').on('click', function () {
         $(".work-content").hide();
         $(".main-home").show();
-        $(".footer-content").hide(); 
+        $(".footer-content").hide();
         $(".footer-home").show();
 
         $("body").css("background-color", "#EF5350");
         $("#line-separator").css("display", "none");
         $("#background-whitish").css("display", "block");
-        $(".js-work-btn").css({"text-decoration-line": "none", "font-style": "normal"});
+        $(".js-work-btn").css({ "text-decoration-line": "none", "font-style": "normal" });
     });
-    
+
     viewProjects();
 }
 
 function viewProjects() {
 
-    $('#hacker-events-project').on('click', function() {
-             
+    $('#hacker-events-project').on('click', function () {
+
         $("#hacker-events-selected").show();
         $("#css-quiz-selected").hide();
         $("#fajitas-ritas-selected").hide();
-        $("#project3-selected").hide();
-    }); 
+        $("#where-to-eat-selected").hide();
+    });
 
-    $('#css-quiz-project').on('click', function() {
-             
+    $('#css-quiz-project').on('click', function () {
+
         $("#css-quiz-selected").show();
         $("#fajitas-ritas-selected").hide();
         $("#hacker-events-selected").hide();
-        $("#project3-selected").hide();
-    }); 
+        $("#where-to-eat-selected").hide();
+    });
 
-    // $('#project3').on('click', function() {
-             
-    //     $("#project3-selected").show();
-    //     $("#css-quiz-selected").hide();
-    //     $("#fajitas-ritas-selected").hide();
-    //     $("#hacker-events-selected").hide();
-    // }); 
-    
-    $('#fajitas-ritas-project').on('click', function() {
-             
+    $('#where-to-eat-project').on('click', function () {
+
+        $("#where-to-eat-selected").show();
+        $("#css-quiz-selected").hide();
+        $("#fajitas-ritas-selected").hide();
+        $("#hacker-events-selected").hide();
+    });
+
+    $('#fajitas-ritas-project').on('click', function () {
+
         $("#fajitas-ritas-selected").show();
         $("#css-quiz-selected").hide();
         $("#hacker-events-selected").hide();
-        $("#project3-selected").hide();
-    }); 
+        $("#where-to-eat-selected").hide();
+    });
 }
 
 
 function handleContactBtn() {
 
-    $('#js-contact-btn').on('click', function() {
+    $('#js-contact-btn').on('click', function () {
 
         $(".footer-home").hide();
         $(".footer-content").show();
 
-        $("#contact-btn").css({"text-decoration-line": "line-through", "font-style": "italic"});      
+        $("#contact-btn").css({ "text-decoration-line": "line-through", "font-style": "italic" });
     });
 
-    $("#contact-close-btn").on('click', function() {
+    $("#contact-close-btn").on('click', function () {
         $(".footer-content").hide();
-        $(".footer-home").show(); 
+        $(".footer-home").show();
     });
 }
 
@@ -159,7 +159,6 @@ function handlePortfolio() {
     handleWorkBtn();
     handleContactBtn();
 }
-  
+
 handlePortfolio();
 
-  
